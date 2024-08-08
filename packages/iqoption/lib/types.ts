18a5d4e07095ca2a7d5853275a94e2930571bd81
André Dezzy <hello@andredezzy.com>
2024-08-08 02:34:35 -0300

    chore: Update package versions and add LogInCredentials interface

--- packages/iqoption/lib/types.ts
+++ packages/iqoption/lib/types.ts
@@ -9,8 +9,6 @@ import {
 import { PositionState } from './websocket/events/responses/PositionsState'
 import { WebSocketClient } from './websocket/WebSocketClient'
 
-// .
-
 export interface LogInCredentials {
   email: string
   password: string
